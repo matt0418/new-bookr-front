f5f1970266528406499e94e8389bf5f6e5f7d7e7
matt0418 <mfeldma7@gmail.com>
2019-05-04 18:09:36 -0400

    fixed some loggin in issues, and added a loader

--- bookr-client/src/views/BookListView.js
+++ bookr-client/src/views/BookListView.js
@@ -7,19 +7,40 @@ import { fetchBooks } from '../store/actions/bookActions'
 import ListOfBooks from '../components/ListOfBooks'
 import NavBar from '../components/NavBar'
 import Jumbo from '../components/Jumbo'
+import Loader from 'react-loader-spinner'
+import styled from 'styled-components'
 
+const LoaderContainer = styled.div`
+    margin-top: 25%;
+`
 
 const BookListView = props => {
 
+    const token = localStorage.getItem('jwt')
     
     useEffect(() => {
         if (localStorage.jwt) {
             props.fetchBooks()
         }
-    },[])
+    },[token])
+
+    if (props.isLoggingIn === true) {
+        return (
+            <LoaderContainer>
+                <h2>You are being logged in</h2>
+                <Loader
+                    color="#689775"
+                    height="100"
+                    width="100%"
+                />
+            </LoaderContainer>
+            
+        )
+
+    }
 
 
-    if (localStorage.jwt !== undefined) {
+    if (props.isLoggingIn === false && localStorage.jwt) {
         return(
             <div>
                 <NavBar />
@@ -40,6 +61,7 @@ const BookListView = props => {
 
 const mapStateToProps = state => ({
     loggedIn: state.credentialsReducer.loggedIn,
+    isLoggingIn: state.credentialsReducer.isLoggingIn,
     isLoggingOut: state.credentialsReducer.isLoggingOut,
     books: state.bookReducer.books
 })
@@ -48,11 +70,6 @@ export default connect(
     mapStateToProps,
     { 
         userLogout,
-        fetchBooks
+        fetchBooks,
      }
 )(BookListView)
-
-// export default requireAuth(connect(
-//     mapStateToProps,
-//     { userLogout }
-// )(BookListView))(LoginView)
